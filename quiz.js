013413f691136883235a02158d23095b23fc2401
Shubham <40598861+vijit101@users.noreply.github.com>
2024-10-24 19:16:50 +0530

    updated Question
    Added the Question text from the object on the screen

--- quiz.js
+++ quiz.js
@@ -1,23 +1,25 @@
-// # main Execution 
+// # main Execution
 
+const questionObj = {
+  category: "Food & Drink",
+  id: "qa-1",
+  correctAnswer: "Three",
+  options: ["Two", "Three ", "Four", "Five"],
+  question: "How many pieces of bun are in a Mcdonald's Big Mac?",
+};
 
-const questionObj = 
-    {
-      category: 'Food & Drink',
-      id: 'qa-1',
-      correctAnswer: 'Three',
-      answers: ['Two', 'Three ', 'Four', 'Five'],
-      question:
-        "How many pieces of bun are in a Mcdonald's Big Mac?",
-    };
+CreateQuestion(questionObj);
 
+//# main Execution
 
-//# main Execution 
+function CreateQuestion(quesObj) {
+  let ques = quesObj.question;
+  let options = quesObj.options;
+  let quizAnswer = quesObj.correctAnswer;
 
-function CreateQuestion(){
+  quizQuestionElement = document.querySelector("#question");
+  quizQuestionElement.innerHTML = ques;
 
 }
 
-function submitAnswer(){
-
-}
+function submitAnswer() {}
